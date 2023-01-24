794a590582af2aa00ce02aae03e07f3ee12036a4
Beryl B <beriebaldwin@gmail.com>
2023-01-24 18:48:57 -0500

    add best seller index controller function

--- controllers/books.js
+++ controllers/books.js
@@ -1,5 +1,21 @@
 import { Profile } from "../models/profile.js"
 import { Book } from "../models/book.js"
+import axios from "axios"
 
 
-export { }
+function bsIndex(req, res) {
+  axios.get (`https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=${process.env.API_KEY}`)
+  .then(apiResponse => {
+    res.json(apiResponse.data)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({ err: err.errmsg })
+  })
+}
+
+
+
+export {
+  bsIndex,
+ }
