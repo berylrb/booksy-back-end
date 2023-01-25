4404c51dc0d2f199c8a3f9734cb24de45f767c79
Beryl B <beriebaldwin@gmail.com>
2023-01-24 19:49:17 -0500

    isolate isbn 10 in console log

--- controllers/books.js
+++ controllers/books.js
@@ -6,6 +6,7 @@ import axios from "axios"
 function bsIndex(req, res) {
   axios.get (`https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=${process.env.API_KEY}`)
   .then(apiResponse => {
+    console.log(apiResponse.data.results[0].isbns[0].isbn10)
     res.json(apiResponse.data)
   })
   .catch(err => {
@@ -13,6 +14,10 @@ function bsIndex(req, res) {
     res.status(500).json({ err: err.errmsg })
   })
 }
+
+// function showBook(req, res) {
+//   axios.get (`https://api.nytimes.com/svc/books/v3/reviews.json?api-key=${process.env.API_KEY}`)
+// }
 
 
 
