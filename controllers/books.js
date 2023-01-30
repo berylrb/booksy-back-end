a6bff53c6e1a3238f217231d3a623e001be9fadb
Beryl B <beriebaldwin@gmail.com>
2023-01-30 14:57:56 -0500

    adjust api links

--- controllers/books.js
+++ controllers/books.js
@@ -4,13 +4,9 @@ import axios from "axios"
 
 
 function bsIndex(req, res) {
-  axios.get (`https://api.nytimes.com/svc/books/v3/lists.json?list=hardcover-fiction&api-key=${process.env.API_KEY}`)
+  // const today = new Date().toISOString().split('T')[0]
+  axios.get (`https://openlibrary.org/trending/weekly.json?details=true`)
   .then(apiResponse => {
-    apiResponse.data.results.forEach(result => {
-      console.log(result.book_details[0].title, result.isbns[0].isbn13)
-    })
-    console.log(apiResponse.data.results[0].isbns[0].isbn10)
-    console.log(apiResponse.data.results[0].id)
     res.json(apiResponse.data)
   })
   .catch(err => {
@@ -19,18 +15,24 @@ function bsIndex(req, res) {
   })
 }
 
+
 function show (req, res) {
-  console.log(req.params)
-  axios.get(`https://api.nytimes.com/svc/books/v3/reviews.json?isbn=9780385547345&api-key=${process.env.API_KEY}`)
+  const { qKey } = req.params
+  console.log(qKey)
+  // axios.get(`https://api.nytimes.com/svc/books/v3/reviews.json?isbn=${isbn}&api-key=${process.env.API_KEY}`)
+  axios.get(`http://openlibrary.org/works/${qKey}/editions.json`)
   .then(response => {
-    Book.findOne({ isbn: response.results[0].isbn13[0] })
-    res.json(response.results[0])
+    console.log('HERE', response.data)
+    // Book.findOne({ isbn: response.results.isbn13 })
+    res.json(response.data)
   })
   .catch(err => {
     console.log(err)
     res.status(500).json({ err: err.errmsg })
   })
 }
+
+// book ratings = http://openlibrary.org/works/OL28914133W/ratings.json
 
 
 
