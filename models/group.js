2af6ccf7cba27e9dbe47c01600a6bc74e7563ec0
Beryl B <beriebaldwin@gmail.com>
2023-02-02 18:14:16 -0500

    finish group schema

--- models/group.js
+++ models/group.js
@@ -2,4 +2,24 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
-const groupSchema = new Schema({
+const groupSchema = new Schema(
+  {
+    groupName: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    imgUrl: { type: String },
+    members: [{ type: Schema.Types.ObjectId, ref: "Profile" }],
+    booksRead: [{ type: Schema.Types.ObjectId, ref: "Book" }],
+  },
+  { timestamps: true },
+  { typeKey: '$type' }
+)
+
+const Group = mongoose.model('Group', groupSchema)
+
+export { Group }
