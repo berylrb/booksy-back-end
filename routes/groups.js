e700f5f2fc41d2c4193b3cb97beeb4d75ed2ba3c
Beryl B <beriebaldwin@gmail.com>
2023-02-02 19:22:39 -0500

    add group delete route

--- routes/groups.js
+++ routes/groups.js
@@ -13,6 +13,7 @@ router.post('/', checkAuth, groupsCtrl.createGroup)
 router.get('/', checkAuth, groupsCtrl.index)
 router.get('/:groupId', checkAuth, groupsCtrl.show)
 router.put('/:groupId', checkAuth, groupsCtrl.update)
+router.delete('/:groupId', checkAuth, groupsCtrl.delete)
 router.put('/:groupId/add-photo', checkAuth, groupsCtrl.addPhoto)
 
 export { router }
